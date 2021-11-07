db71012367f7d95cd6adcbd889879c36d29bfc85
José Carlos <jcnv.trabalho@gmail.com>
2021-11-07 18:23:51 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormTableComponent } from './form-table/form-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatTableModule,
+    MatIconModule,
+    MatTooltipModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-table/form-table.component.ts
@@ -0,0 +1,106 @@
+import { DataSource } from '@angular/cdk/collections';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Observable, ReplaySubject } from 'rxjs';
+
+export interface formulario {
+  nome: any;
+  idade: any;
+  teste1: any;
+  teste2: any;
+  teste3: any;
+}
+
+const elementdata: formulario[] = [];
+
+@Component({
+  selector: 'app-form-table',
+  templateUrl: './form-table.component.html',
+  styleUrls: ['./form-table.component.css']
+})
+export class FormTableComponent implements OnInit {
+  displayedColumns: string[] = ['Nome', 'Idade', 'Teste1','Teste2', 'Teste3']
+  public nome = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  public idade = new FormControl('', [Validators.required, Validators.min(100), this.validarQtd]);
+  public teste1 = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  public teste2 = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  public teste3 = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  dataToDisplay = [...elementdata];
+  dataSource = new ExampleDataSource(this.dataToDisplay);
+  newAsset: boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  validarQtd(input: FormControl)
+  {
+    return ((input.value%100==0) ? null : {validar: true})
+  }
+
+  getErrorMessage() {
+    if (this.idade.hasError('required')) {
+      return 'Precisa digitar um valor';
+    }
+    return null
+  }
+
+  mudarOperacao() {
+    if(this.newAsset == true)
+    {this.newAsset = false}
+    else {this.newAsset=true}
+  }
+
+  addData() {
+    if (this.newAsset == false)
+    {
+      if (!this.nome.invalid && !this.idade.invalid && this.idade.value%100 == 0)
+      {
+        const newValue = {nome: this.nome.value, idade: this.idade.value, teste1: null, teste2: null, teste3: null};
+        this.dataToDisplay.push(newValue);
+        this.dataSource.setData(this.dataToDisplay);
+        this.nome.reset();
+        this.idade.reset();
+      }
+    }
+    else if (this.newAsset == true)
+    {
+      if (!this.nome.invalid && !this.idade.invalid && this.idade.value%100 == 0)
+      {
+        const newValue = {nome: this.nome.value, idade: this.idade.value, teste1: this.teste1.value, teste2: this.teste2.value, teste3: this.teste3.value};
+        this.dataToDisplay.push(newValue);
+        this.dataSource.setData(this.dataToDisplay);
+        this.nome.reset();
+        this.idade.reset();
+        this.teste1.reset();
+        this.teste2.reset();
+        this.teste3.reset();
+      }
+    }
+  }
+}
+
+/* OUTRA CLASSE */
+
+class ExampleDataSource extends DataSource<formulario> {
+  private _dataStream = new ReplaySubject<formulario[]>();
+
+  constructor(initialData: formulario[]) {
+    super();
+    this.setData(initialData);
+  }
+
+  connect(): Observable<formulario[]> {
+    return this._dataStream;
+  }
+
+  disconnect() {}
+
+  setData(data: formulario[]) {
+    this._dataStream.next(data);
+  }
+}
+
